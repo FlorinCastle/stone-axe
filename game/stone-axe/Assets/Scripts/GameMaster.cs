2d16cb3c073486d244da6d3ee65bb95264317db4
Kaytra <86370363+Kaytra@users.noreply.github.com>
2021-08-19 11:11:15 -0700

    ui coding

---
+++ game/stone-axe/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ game/stone-axe/Assets/Scripts/UIControl.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] GameObject mainMenuUI;
+    [SerializeField] GameObject gameShopUI;
+    [SerializeField] GameObject economicSubUI;
+    [SerializeField] GameObject disassembleSubUI;
+    [SerializeField] GameObject craftSubUI;
+
+    private void Awake()
+    {
+        // check if ui objects have something assigned
+        if (mainMenuUI == null)
+            Debug.LogError("Main Menu UI is not assigned");
+        if (gameShopUI == null)
+            Debug.LogError("Game Shop UI is not assigned");
+        if (economicSubUI == null)
+            Debug.LogError("Economic Sub UI UI is not assigned");
+        if (disassembleSubUI == null)
+            Debug.LogError("Disassemble Sub UI UI is not assigned");
+        if (craftSubUI == null)
+            Debug.LogError("Craft Sub UI UI is not assigned");
+    }
+
+    public void unloadUI(GameObject UIInput)
+    {
+        UIInput.SetActive(false);
+    }
+
+    public void loadUI(GameObject UIInput)
+    {
+        UIInput.SetActive(true);
+    }
+}
