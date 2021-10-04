c1dc98622a533d357b79c8919b1727f346bc91c0
Kaytra <86370363+Kaytra@users.noreply.github.com>
2021-10-04 09:21:54 -0700

    recipe UI work
    started work on recipe book ui
    UI work complete
    started work on code
    UI populates with Item Recipes
    TODO
    complete Item Recipe text detail code
    do Part Recipe code

--- game/stone-axe/Assets/Scripts/Items/RecipeBook.cs
+++ game/stone-axe/Assets/Scripts/Items/RecipeBook.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RecipeBook : MonoBehaviour
 {
@@ -8,6 +9,104 @@ public class RecipeBook : MonoBehaviour
     [SerializeField] private List<PartData> partRecipes;
     [SerializeField, HideInInspector] private List<string> itemRecipeName;
     [SerializeField, HideInInspector] private List<string> partRecipeName;
+    [SerializeField, HideInInspector] private List<GameObject> recipeButtons;
+    [Header("UI References")]
+    [SerializeField] private Text _recipeText;
+    [SerializeField] private Button _recipeSelectButton;
+    [SerializeField] private GameObject _contentRef;
+    [Header("Prefabs")]
+    [SerializeField] private GameObject _itemRecipeInfoPrefab;
+    [SerializeField] private GameObject _partRecipeInfoPrefab;
+
+    private void Awake()
+    {
+        _recipeSelectButton.interactable = false; // temp till I get all the code set up
+        recipeButtons = new List<GameObject>();
+        setupRecipeGrid();
+    }
+
+    public void setItemRecipeInfo(int index)
+    {
+        // get gameobject from recipeButton list at input index
+        if (index == -1)
+        {
+            _recipeText.text = "placeholder";
+        }
+        else
+        {
+            GameObject button = recipeButtons[index];
+            foreach (string itemRecipe in itemRecipeName)
+            {
+                if (itemRecipe == button.GetComponent<RecipeButton>().GetRecipeName)
+                {
+                    _recipeText.text = "";
+                }
+            }
+
+        }
+    }
+
+    public void setPartRecipeInfo(int index)
+    {
+
+    }
+
+    private GameObject tempButton;
+    public void setupRecipeGrid()
+    {
+        clearRecipeGrid();
+        int r = 0;
+        foreach (ItemData itemRecipe in itemRecipes)
+        {
+            if (itemRecipe != null)
+            {
+                // instantiate the button prefab
+                tempButton = Instantiate(_itemRecipeInfoPrefab);
+                tempButton.transform.SetParent(_contentRef.transform, false);
+                tempButton.GetComponent<RecipeButton>().setRecipeName(itemRecipe.ItemName);
+                // set up button text
+                Text t = tempButton.GetComponentInChildren<Text>();
+                t.text = itemRecipe.ItemName + " Recipe";
+                tempButton.name = itemRecipe.ItemName + " Recipe";
+                // add button to list
+                InsertButton(tempButton, r);
+            }
+            r++;
+        }
+
+        foreach (PartData partRecipe in partRecipes)
+        {
+            if (partRecipe != null)
+            {
+                tempButton = Instantiate(_partRecipeInfoPrefab);
+                tempButton.transform.SetParent(_contentRef.transform, false);
+                tempButton.GetComponent<RecipeButton>().setRecipeName(partRecipe.PartName);
+
+                Text t = tempButton.GetComponentInChildren<Text>();
+                t.text = partRecipe.PartName + " Recipe";
+                tempButton.name = partRecipe.PartName + " Recipe";
+                // add button to list
+                InsertButton(tempButton, r);
+            }
+            r++;
+        }
+    }
+
+    private void clearRecipeGrid()
+    {
+        foreach (GameObject go in recipeButtons)
+            Destroy(go);
+
+        for (int r = 0; r < recipeButtons.Count; r++)
+            recipeButtons.RemoveAt(r);
+    }
+
+    private int InsertButton(GameObject button, int index)
+    {
+        recipeButtons.Add(button);
+        button.GetComponent<RecipeButton>().setMyIndex(index);
+        return index;
+    }
 
 
     public List<string> itemRecipesNames()
---
+++ game/stone-axe/Assets/Scripts/RecipeButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeButton : MonoBehaviour
+{
+    [SerializeField] private RecipeBook _recipeControl;
+    [SerializeField, HideInInspector] private int _myButtonIndex;
+    [SerializeField, HideInInspector] private string _recipeName;
+
+    private void Awake()
+    {
+        _recipeControl = GameObject.FindGameObjectWithTag("RecipeBookControl").GetComponent<RecipeBook>();
+    }
+
+    public void setItemRecipeInfoText()
+    {
+        _recipeControl.setItemRecipeInfo(_myButtonIndex);
+    }
+
+    public void setPartRecipeInfoText()
+    {
+        _recipeControl.setPartRecipeInfo(_myButtonIndex);
+    }
+
+    public void setRecipeName(string name) { _recipeName = name; }
+    public string GetRecipeName { get => _recipeName; }
+
+    public void setMyIndex(int value) { _myButtonIndex = value; }
+    public int GetMyIndex { get => _myButtonIndex; }
+}
