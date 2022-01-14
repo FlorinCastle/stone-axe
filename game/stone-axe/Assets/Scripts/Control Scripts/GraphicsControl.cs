490af9f946809d1ec543fff95d0c46eecf3d2a71
Kaytra <88939082+FlorinKatherine@users.noreply.github.com>
2022-01-14 11:54:33 -0800

    settings
    more settings work
    - main menu now has settings control
    - settings settings carries over between ig settings menu and main menu settings menu

--- game/stone-axe/Assets/Scripts/Control Scripts/GraphicsControl.cs
+++ game/stone-axe/Assets/Scripts/Control Scripts/GraphicsControl.cs
@@ -5,10 +5,20 @@ using UnityEngine.UI;
 
 public class GraphicsControl : MonoBehaviour
 {
+    [Header("Main Menu UI")]
+    [SerializeField] private Dropdown _menuResolutionDropdown;
+    [SerializeField] private Dropdown _menuQualityDropdown;
+    [SerializeField] private Dropdown _menuTextureDropdown;
+    [SerializeField] private Dropdown _aaMenuDropdown;
+    [SerializeField] private Toggle _menuFullscreenToggle;
+
+    [Header("In Game UI")]
     [SerializeField] private Dropdown _resolutionDropdown;
     [SerializeField] private Dropdown _qualityDropdown;
     [SerializeField] private Dropdown _textureDropdown;
     [SerializeField] private Dropdown _aaDropdown;
+    [SerializeField] private Toggle _fullscreenToggle;
+
     [SerializeField] private Resolution[] resolutions;
 
 
@@ -16,6 +26,7 @@ public class GraphicsControl : MonoBehaviour
     void Start()
     {
         _resolutionDropdown.ClearOptions();
+        _menuResolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
         resolutions = Screen.resolutions;
         int currentResolutionIndex = 0;
@@ -28,7 +39,9 @@ public class GraphicsControl : MonoBehaviour
                 currentResolutionIndex = i;
         }
         _resolutionDropdown.AddOptions(options);
+        _menuResolutionDropdown.AddOptions(options);
         _resolutionDropdown.RefreshShownValue();
+        _menuResolutionDropdown.RefreshShownValue();
         LoadSettings(currentResolutionIndex);
     }
 
@@ -68,16 +81,24 @@ public class GraphicsControl : MonoBehaviour
     {
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        _resolutionDropdown.value = resolutionIndex;
+        _menuResolutionDropdown.value = resolutionIndex;
     }
     public void SetTextureQuality(int textureIndex)
     {
         QualitySettings.masterTextureLimit = textureIndex;
         _qualityDropdown.value = 6;
+        _menuQualityDropdown.value = 6;
+        _textureDropdown.value = textureIndex;
+        _menuTextureDropdown.value = textureIndex;
     }
     public void SetAntiAliasing(int aaIndex)
     {
         QualitySettings.antiAliasing = aaIndex;
         _qualityDropdown.value = 6;
+        _menuQualityDropdown.value = 6;
+        _aaDropdown.value = aaIndex;
+        _aaMenuDropdown.value = aaIndex;
     }
     public void SetQuality(int qualityIndex)
     {
@@ -87,31 +108,48 @@ public class GraphicsControl : MonoBehaviour
         {
             case 0: // quality level - very low
                 _textureDropdown.value = 3;
+                _menuTextureDropdown.value = 3;
                 _aaDropdown.value = 0;
+                _aaMenuDropdown.value = 0;
                 break;
             case 1: // quality level - low
                 _textureDropdown.value = 2;
+                _menuTextureDropdown.value = 2;
                 _aaDropdown.value = 0;
+                _aaMenuDropdown.value = 0;
                 break;
             case 2: // quality level - medium
                 _textureDropdown.value = 1;
+                _menuTextureDropdown.value = 1;
                 _aaDropdown.value = 0;
+                _aaMenuDropdown.value = 0;
                 break;
             case 3: // quality level - high
                 _textureDropdown.value = 0;
+                _menuTextureDropdown.value = 0;
                 _aaDropdown.value = 0;
+                _aaMenuDropdown.value = 0;
                 break;
             case 4: // quality level - very high
                 _textureDropdown.value = 0;
+                _menuTextureDropdown.value = 0;
                 _aaDropdown.value = 1;
+                _aaMenuDropdown.value = 1;
                 break;
             case 5: // quality level - ultra
                 _textureDropdown.value = 0;
+                _menuTextureDropdown.value = 0;
                 _aaDropdown.value = 2;
+                _aaMenuDropdown.value = 2;
                 break;
         }
         _qualityDropdown.value = qualityIndex;
+        _menuQualityDropdown.value = qualityIndex;
     }
 
-    public void SetFullscreen (bool isFullscreen) { Screen.fullScreen = isFullscreen; }
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        _fullscreenToggle.isOn = isFullscreen; _menuFullscreenToggle.isOn = isFullscreen;
+    }
 }
--- game/stone-axe/Assets/Scripts/SoundScripts/SetVolume.cs
+++ game/stone-axe/Assets/Scripts/SoundScripts/SetVolume.cs
@@ -8,9 +8,13 @@ public class SetVolume : MonoBehaviour
 {
     [SerializeField] private AudioMixer _mixer;
 
+    [Header("Game")]
     [Header("UI")]
     [SerializeField] private Slider _musicSlider;
     [SerializeField] private Slider _sfxSlider;
+    [Header("")]
+    [SerializeField] private Slider _menuMusicSlider;
+    [SerializeField] private Slider _menuSfxSlider;
 
     private void Start()
     {
@@ -23,13 +27,17 @@ public class SetVolume : MonoBehaviour
 
     public void SetLevel(float sliderValue, Slider sliderRef)
     {
-        if (sliderRef == _musicSlider)
+        if (sliderRef == _musicSlider || sliderRef == _menuMusicSlider)
         {
+            if (sliderRef == _musicSlider) _menuMusicSlider.value = sliderValue;
+            else if (sliderRef == _menuMusicSlider) _musicSlider.value = sliderValue;
             _mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
             PlayerPrefs.SetFloat("MusicVolume", sliderValue);
         }
-        if (sliderRef == _sfxSlider)
+        if (sliderRef == _sfxSlider || sliderRef == _menuSfxSlider)
         {
+            if (sliderRef == _sfxSlider) _menuSfxSlider.value = sliderValue;
+            else if (sliderRef == _menuSfxSlider) _sfxSlider.value = sliderValue;
             _mixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
             PlayerPrefs.SetFloat("SFXVolume", sliderValue);
         }
